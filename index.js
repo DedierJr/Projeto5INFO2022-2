241e7726cc1a5058a7121c3d0b6dda0674c7fcb3
DedierJr <103050760+DedierJr@users.noreply.github.com>
2023-02-27 13:41:33 -0300

    Adicionando a rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/msg', function(req,res) {
     res.send("Essa mensagem é automática!")
 })
 
+app.get('/spbre', function(req,res) {
+    res.send("Esta página está sendo desenvolvida por Dedier!")
+})
+
 app.listen(3000,function() {
     console.log("Conexão inicializada")
 })
