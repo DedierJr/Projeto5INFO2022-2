26d163ce6d26765b43104f55346129e3f009dcc0
DedierJr <103050760+DedierJr@users.noreply.github.com>
2023-03-07 11:28:38 -0300

    Create post.js

---
+++ model/post.js
@@ -0,0 +1,16 @@
+const conexao = require('../config/conexao');
+
+const postSchema = new mongoose.Schema({
+    titulo:  String,
+    texto: String,
+    data: Date,
+    foto: String,
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'usuario'
+    }
+});
+
+const post = conexao.model('post', postSchema);
+
+module.exports = post
